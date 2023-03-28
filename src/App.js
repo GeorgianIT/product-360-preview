3d04536b5874912a6093daedd88bc33ada2b486c
GeorgianIT <iulian.tudor2405@gmail.com>
2023-03-28 12:53:18 +0300

    finish the laptop preview

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import ProductView from './components/ProductView';
 
 function App() {
   return (
     <div className="App">
-asd
+      <ProductView/>
     </div>
   );
 }
---
+++ src/components/ProductView.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import  laptop  from '../assets/LaptopGif2.gif'
+
+const ProductView = () => {
+  return (
+    <div>
+      <img src={laptop} />
+      <h1>Is not what i expected, but is the easiest way to do it</h1>
+    </div>
+
+  )
+}
+
+export default ProductView
